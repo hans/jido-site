ef330a5f7f6f7a650fb74b8d475a294853b27bc7
Hans Engel <engel@engel.uk.to>
2011-02-16 07:49:53 -0700

    added basic conjugator functionality

--- jido.rb
+++ jido.rb
@@ -2,6 +2,8 @@ require 'rubygems'
 require 'sinatra'
 
 require 'haml'
+require 'json'
+require 'jido'
 
 get '/' do
     haml :index
@@ -10,4 +12,17 @@ end
 get '/style.css' do
   content_type 'text/css', :charset => 'utf-8'
   sass :style
+end
+
+conjugators = {}
+get '/conjugate/:lang/:verb' do
+    content_type :json
+    
+    conjugator = nil
+    if conjugators[params[:lang]].nil?
+	conjugator = Jido.load params[:lang]
+	conjugators[params[:lang]] = conjugator
+    else conjugator = conjugators[params[:lang]] end
+
+    conjugator.conjugate(params[:verb]).to_json
 end
