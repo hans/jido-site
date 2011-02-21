b238650d8129729b6b126ee82df2e30d203baff0
Hans Engel <engel@engel.uk.to>
2011-02-20 22:08:42 -0700

    added Redis caching

--- jido.rb
+++ jido.rb
@@ -4,6 +4,7 @@ require 'sinatra'
 require 'haml'
 require 'json'
 require 'jido'
+require 'redis'
 
 get '/' do
     haml :index
@@ -14,15 +15,37 @@ get '/style.css' do
   sass :style
 end
 
+# Store conjugator objects so they only need to be initialized once
 conjugators = {}
+
+# Probably shouldn't show you the DB info :)
+db = open('redis_info.json') do |f|
+  JSON.parse f
+end
+
+# Connect to the Redis instance.
+# This will be used to cache conjugation results.
+cache = Redis.new :host => db['host'], :port => db['port'], :password => db['password']
+
 get '/conjugate/:lang/:verb' do
-    content_type :json
-    
+  content_type :json
+  
+  cache_query = "verbs:#{params[:lang]}:#{params[:verb]}"
+  cached = cache.get cache_query
+  
+  if cached.nil?
     conjugator = nil
     if conjugators[params[:lang]].nil?
-	conjugator = Jido.load params[:lang]
-	conjugators[params[:lang]] = conjugator
-    else conjugator = conjugators[params[:lang]] end
-
-    conjugator.conjugate(params[:verb]).to_json
+      conjugator = Jido.load params[:lang]
+      conjugators[params[:lang]] = conjugator
+    else
+      conjugator = conjugators[params[:lang]]
+    end
+    
+    ret = conjugator.conjugate(params[:verb]).to_json
+    cache.set cache_query, ret
+    return ret
+  else
+    return cached
+  end
 end
