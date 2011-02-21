2172f36e396887e0ca22dc6116f9e43447f6744f
Hans Engel <engel@engel.uk.to>
2011-02-20 22:28:36 -0700

    moved caching stuff to a configure block

--- jido.rb
+++ jido.rb
@@ -6,6 +6,15 @@ require 'json'
 require 'jido'
 require 'redis'
 
+configure do
+  # Store conjugator objects so they only need to be initialized once
+  @@conjugators = {}
+
+  # Connect to the Redis instance.
+  # This will be used to cache conjugation results.
+  @@cache = Redis.new :host => ENV['REDIS_HOST'], :port => ENV['REDIS_PORT'].to_i, :password => ENV['REDIS_PASSWORD']
+end
+
 get '/' do
     haml :index
 end
@@ -15,32 +24,25 @@ get '/style.css' do
   sass :style
 end
 
-# Store conjugator objects so they only need to be initialized once
-conjugators = {}
-
-# Connect to the Redis instance.
-# This will be used to cache conjugation results.
-cache = Redis.new :host => ENV['REDIS_HOST'], :port => ENV['REDIS_PORT'].to_i, :password => ENV['REDIS_PASSWORD']
-
 get '/conjugate/:lang/:verb' do
   content_type :json
   
   cache_query = "verbs:#{params[:lang]}:#{params[:verb]}"
-  cached = cache.exists cache_query
+  cached = @@cache.exists cache_query
   
   if cached
-    return cache.get cache_query
+    return @@cache.get cache_query
   else
     conjugator = nil
-    if conjugators[params[:lang]].nil?
+    if @@conjugators[params[:lang]].nil?
       conjugator = Jido.load params[:lang]
-      conjugators[params[:lang]] = conjugator
+      @@conjugators[params[:lang]] = conjugator
     else
-      conjugator = conjugators[params[:lang]]
+      conjugator = @@conjugators[params[:lang]]
     end
     
     ret = conjugator.conjugate(params[:verb]).to_json
-    cache.set cache_query, ret
+    @@cache.set cache_query, ret
     return ret
   end
 end
